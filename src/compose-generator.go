0a7d0e6db792ae803cbe3ee354c8cc28e7a8ca55
Marc Auberer <marc.auberer@chillibits.com>
2021-01-23 01:23:27 +0100

    Add subcommands and version flag

--- src/compose-generator.go
+++ src/compose-generator.go
@@ -8,13 +8,51 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const VERSION = "1.0.0"
+
 func main() {
+	// Version flag
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"v"},
+		Usage:   "Prints the version of compose-generator",
+	}
+
+	// Main cli configuration
 	app := &cli.App{
-		Name:  "generate",
-		Usage: "Generate a docker compose configuration file",
+		Name:    "compose-generator",
+		Version: VERSION,
+		Authors: []*cli.Author{
+			{Name: "Marc Auberer", Email: "marc.auberer@chillibits.com"},
+		},
+		Copyright: "© 2021 Marc Auberer",
+		Usage:     "Generate docker compose configuration files for your projects.",
 		Action: func(c *cli.Context) error {
-			fmt.Print("What is the name of your project: ")
+			generate()
 			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{Name: "advanced", Aliases: []string{"a"}, Usage: "Generate compose file in advanced mode"},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "Adds a service to an existing compose file",
+				Action: func(c *cli.Context) error {
+					add()
+					return nil
+				},
+			},
+			{
+				Name:    "remove",
+				Aliases: []string{"r", "rm"},
+				Usage:   "Removes a service from an existing compose file",
+				Action: func(c *cli.Context) error {
+					remove()
+					return nil
+				},
+			},
 		},
 	}
 
@@ -22,4 +60,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func generate() {
+	fmt.Print("What is the name of your project: ")
+}
+
+func add() {
+
+}
+
+func remove() {
+
 }
