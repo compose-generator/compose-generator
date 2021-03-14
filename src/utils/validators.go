bc011392243721e14625c5fc1198596871c1e28b
Marc Auberer <marc.auberer@chillibits.com>
2021-03-14 20:12:15 +0000

    Code cleanup

--- src/utils/validators.go
+++ src/utils/validators.go
@@ -9,38 +9,10 @@ import (
 
 var validate *validator.Validate
 
-func Validator() {
-
-}
-
-// NoSpacesValidator is a validator to check if a string contains space chars
-func NoSpacesValidator(val interface{}) error {
-	// TODO: Implement validator
-	return nil
-}
-
-// EmailValidator is a validator to check if an email is valid
-func EmailValidator(val interface{}) error {
-	initValidator()
-	if validate.Var(val.(string), "required,email") != nil {
-		return errors.New("please provide a valid email address")
-	}
-	return nil
-}
-
 // PortValidator is a validator function to check if a port number is valid
 func PortValidator(val interface{}) error {
 	if number, err := strconv.Atoi(val.(string)); err != nil || number < 0 || number > 65535 {
 		return errors.New("please provide an integer value between 0 and 65535")
-	}
-	return nil
-}
-
-// HostNameValidator is a validator to check if a hostname is valid.
-func HostNameValidator(val interface{}) error {
-	initValidator()
-	if validate.Var(val.(string), "required,hostname") != nil {
-		return errors.New("please provide a valid hostname")
 	}
 	return nil
 }
