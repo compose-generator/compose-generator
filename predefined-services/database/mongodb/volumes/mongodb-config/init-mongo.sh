6cbc2bc622030fe32e7c5faaf79b9983f93deb07
Marc Auberer <marc.auberer@chillibits.com>
2021-04-12 23:16:45 +0000

    Specify readWrite permissions for app user on app db

--- predefined-services/database/mongodb/volumes/mongodb-config/init-mongo.sh
+++ predefined-services/database/mongodb/volumes/mongodb-config/init-mongo.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
-set -e
-
+#!/bin/bash
 mongo <<EOF
 use $MONGO_INITDB_DATABASE
-
-db.createUser(
-    {
-        user: "$MONGODB_APPLICATION_USER",
-        password: "$MONGODB_APPLICATION_USER_PW"
-    }
-)
+db.createUser({
+    user: "$MONGODB_APPLICATION_USER",
+    password: "$MONGODB_APPLICATION_USER_PW",
+    roles: [{
+        role: 'readWrite',
+        db: '$MONGO_INITDB_DATABASE'
+    }]
+})
 EOF
