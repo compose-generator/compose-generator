eecd6fca3d26353626fe8ed25a304e997d1ad36f
Marc Auberer <marc.auberer@chillibits.com>
2021-04-12 23:35:20 +0000

    Fix init script

--- predefined-services/database/mongodb/volumes/mongodb-init/init-mongo.sh
+++ predefined-services/database/mongodb/volumes/mongodb-init/init-mongo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
+
 mongo <<EOF
 use $MONGO_INITDB_DATABASE
 db.createUser({
     user: "$MONGODB_APPLICATION_USER",
-    password: "$MONGODB_APPLICATION_USER_PW",
+    pwd: "$MONGODB_APPLICATION_USER_PW",
     roles: [{
         role: 'readWrite',
         db: '$MONGO_INITDB_DATABASE'
