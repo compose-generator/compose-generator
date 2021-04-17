7e1ffbc6e2057e62974f6a9beb57ca677c4c2223
Marc Auberer <marc.auberer@chillibits.com>
2021-04-17 17:12:22 +0000

    Extend README

--- src/cmd/generate.go
+++ src/cmd/generate.go
@@ -415,8 +415,10 @@ func processUserInput(
 						for _, template := range templateData["backend"] {
 							service.DependsOn = append(service.DependsOn, "backend-"+template.Name)
 						}
-						for _, template := range templateData["database"] {
-							service.DependsOn = append(service.DependsOn, "database-"+template.Name)
+						if len(templateData["backend"]) == 0 {
+							for _, template := range templateData["database"] {
+								service.DependsOn = append(service.DependsOn, "database-"+template.Name)
+							}
 						}
 					case "backend":
 						service.DependsOn = []string{}
