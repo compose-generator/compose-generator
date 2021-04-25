f39f9c7fbb51537f4377c26da6a2990cb91a4a25
Marc Auberer <marc.auberer@chillibits.com>
2021-04-25 18:29:28 +0000

    Add more config options

--- predefined-services/frontend/nextcloud/volumes/nextcloud-config/config.php
+++ predefined-services/frontend/nextcloud/volumes/nextcloud-config/config.php
@@ -2,5 +2,5 @@
 $CONFIG = array (
   'trusted_proxies'   => ['127.0.0.1'],
   'overwritehost'     => '${{NEXTCLOUD_OUTSIDE_HOST}}',
-  'overwriteprotocol' => '${{NEXTCLOUD_OUTSIDE_PROTOCOL}}'
+  'overwriteprotocol' => '${{NEXTCLOUD_OUTSIDE_SCHEME}}'
 );
