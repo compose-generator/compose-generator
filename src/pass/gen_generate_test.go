dd88cdb220bb8de7a09b3940bc354959a74637a3
Marc Auberer <marc.auberer@chillibits.com>
2021-09-10 21:12:18 +0000

    Add more tests

--- src/pass/gen_generate_test.go
+++ src/pass/gen_generate_test.go
@@ -2,16 +2,24 @@ package pass
 
 import (
 	"compose-generator/model"
+	"compose-generator/project"
 	"testing"
 
 	"github.com/briandowns/spinner"
+	spec "github.com/compose-spec/compose-go/types"
 	"github.com/stretchr/testify/assert"
 )
+
+// --------------------------------------------- Generate ----------------------------------------------
 
 func TestGenerate1(t *testing.T) {
 	// Test data
 	predefinedTemplatesPath := "../predefined-templates"
-	project := &model.CGProject{}
+	proj := &model.CGProject{
+		CGProjectMetadata: model.CGProjectMetadata{
+			WithReadme: true,
+		},
+	}
 	selectedTemplates := &model.SelectedTemplates{
 		FrontendServices: []model.PredefinedTemplateConfig{
 			{
@@ -22,6 +30,18 @@ func TestGenerate1(t *testing.T) {
 			{
 				Name: "nginx",
 			},
+		},
+	}
+	expectedProj := &model.CGProject{
+		CGProjectMetadata: model.CGProjectMetadata{
+			WithReadme: true,
+		},
+		ReadmeChildPaths: []string{
+			predefinedTemplatesPath + "/INSTRUCTIONS_HEADER.md",
+		},
+		Composition: &spec.Project{
+			WorkingDir: "./",
+			Services:   spec.Services{},
 		},
 	}
 	// Mock functions
@@ -60,10 +80,11 @@ func TestGenerate1(t *testing.T) {
 		assert.Fail(t, "Unexpected call of printError")
 	}
 	// Execute test
-	Generate(project, selectedTemplates)
+	Generate(proj, selectedTemplates)
 	// Assert
 	assert.Equal(t, 1, pelCallCount)
 	assert.Equal(t, 1, stopProcessCallCount)
+	assert.Equal(t, expectedProj, proj)
 }
 
 func TestGenerate2(t *testing.T) {
@@ -88,4 +109,74 @@ func TestGenerate2(t *testing.T) {
 	Generate(project, selectedTemplates)
 	// Assert
 	assert.Equal(t, 1, pelCallCount)
+}
+
+// ------------------------------------------ generateService ------------------------------------------
+
+func TestGenerateService1(t *testing.T) {
+	// Test data
+	proj := &model.CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{},
+		},
+	}
+	selectedTemplates := &model.SelectedTemplates{}
+	template := model.PredefinedTemplateConfig{
+		Name:  "faunadb",
+		Label: "FaunaDB",
+		Files: []model.File{
+			{
+				Path: "./docs.md",
+				Type: "docs",
+			},
+			{
+				Path: "./env.env",
+				Type: "env",
+			},
+			{
+				Path: "./env.env",
+				Type: "env",
+			},
+		},
+	}
+	templateType := model.TemplateTypeDatabase
+	serviceName := "faunadb"
+	expectedProj := &model.CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					Name: "faunadb",
+				},
+			},
+		},
+		ReadmeChildPaths: []string{
+			"docs.md",
+		},
+		GitignorePatterns: []string{
+			"./env.env",
+		},
+	}
+	// Mock function
+	loadTemplateService = func(project *model.CGProject, selectedTemplates *model.SelectedTemplates, templateTypeName, serviceName string, options ...project.LoadOption) *spec.ServiceConfig {
+		assert.Equal(t, 2, len(options))
+		return &spec.ServiceConfig{
+			Name: "faunadb",
+		}
+	}
+	sliceContainsStringCallCount := 0
+	sliceContainsString = func(slice []string, i string) bool {
+		sliceContainsStringCallCount++
+		assert.Equal(t, "./env.env", i)
+		if sliceContainsStringCallCount == 1 {
+			assert.Nil(t, slice)
+			return false
+		} else {
+			assert.EqualValues(t, []string{"./env.env"}, slice)
+			return true
+		}
+	}
+	// Execute test
+	generateService(proj, selectedTemplates, template, templateType, serviceName)
+	// Assert
+	assert.Equal(t, expectedProj, proj)
 }
