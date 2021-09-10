b8976ed9ca588fa0f85db351bb993cbb09de10c1
Marc Auberer <marc.auberer@chillibits.com>
2021-09-10 19:40:47 +0000

    Add more tests

--- src/pass/remove_volumes_test.go
+++ src/pass/remove_volumes_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRemoveVolumes(t *testing.T) {
+// ------------------------------------------- RemoveVolumes -------------------------------------------
+
+func TestRemoveVolumes1(t *testing.T) {
 	// Test data
 	service := &spec.ServiceConfig{
 		Name: "current-service",
@@ -63,4 +65,155 @@ func TestRemoveVolumes(t *testing.T) {
 	// Assert
 	assert.Equal(t, 2, fileExistsCallCount)
 	assert.Equal(t, expectedProject, project)
+}
+
+func TestRemoveVolumes2(t *testing.T) {
+	// Test data
+	service := &spec.ServiceConfig{
+		Name: "current-service",
+		Volumes: []spec.ServiceVolumeConfig{
+			{
+				Source: "./volumes/test-volume",
+			},
+			{
+				Source: "test",
+			},
+		},
+	}
+	project := &model.CGProject{
+		Composition: &spec.Project{
+			Volumes: spec.Volumes{
+				"test": {},
+			},
+		},
+	}
+	expectedProject := &model.CGProject{
+		Composition: &spec.Project{
+			Volumes: spec.Volumes{
+				"test": {},
+			},
+		},
+	}
+	// Mock functions
+	yesNoQuestion = func(question string, defaultValue bool) bool {
+		assert.Equal(t, "Do you really want to delete all attached volumes of 'current-service' on disk?", question)
+		assert.False(t, defaultValue)
+		return false
+	}
+	fileExistsCallCount := 0
+	fileExists = func(path string) bool {
+		fileExistsCallCount++
+		return false
+	}
+	isVolumeUsedByOtherServicesMockable = func(volume *spec.ServiceVolumeConfig, serv *spec.ServiceConfig, proj *model.CGProject) bool {
+		if fileExistsCallCount == 1 {
+			assert.Equal(t, service.Volumes[0], *volume)
+		} else {
+			assert.Equal(t, service.Volumes[1], *volume)
+		}
+		assert.Equal(t, service, serv)
+		assert.Equal(t, project, proj)
+		return false
+	}
+	removeAll = func(path string) error {
+		assert.Fail(t, "Unexpected call of removeAll")
+		return nil
+	}
+	// Execute test
+	RemoveVolumes(service, project)
+	// Assert
+	assert.Equal(t, 2, fileExistsCallCount)
+	assert.Equal(t, expectedProject, project)
+}
+
+// ------------------------------------ isVolumeUsedByOtherServices ------------------------------------
+
+func TestIsVolumeUsedByOtherServices1(t *testing.T) {
+	// Test data
+	volume := &spec.ServiceVolumeConfig{
+		Source: "./volumes/frontend-react",
+	}
+	service := &spec.ServiceConfig{
+		Name: "current-service",
+	}
+	project := &model.CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					Name: "other-service",
+					Volumes: []spec.ServiceVolumeConfig{
+						{
+							Source: "./volumes/../volumes/frontend-react",
+						},
+						{
+							Source: "../random-other.file",
+						},
+					},
+				},
+				{
+					Name: "current-service",
+				},
+			},
+		},
+	}
+	// Execute test
+	result := isVolumeUsedByOtherServices(volume, service, project)
+	// Assert
+	assert.True(t, result)
+}
+
+func TestIsVolumeUsedByOtherServices2(t *testing.T) {
+	// Test data
+	volume := &spec.ServiceVolumeConfig{
+		Source: "./volumes/database-orientdb",
+	}
+	service := &spec.ServiceConfig{}
+	project := &model.CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					Name: "other-service",
+					Volumes: []spec.ServiceVolumeConfig{
+						{
+							Source: "../random-other.file",
+						},
+						{
+							Source: "./volumes/database-orientdb/config",
+						},
+					},
+				},
+				{
+					Name: "current-service",
+				},
+			},
+		},
+	}
+	// Execute test
+	result := isVolumeUsedByOtherServices(volume, service, project)
+	// Assert
+	assert.True(t, result)
+}
+
+func TestIsVolumeUsedByOtherServices3(t *testing.T) {
+	// Test data
+	volume := &spec.ServiceVolumeConfig{
+		Source: "./volumes/backend-gin",
+	}
+	service := &spec.ServiceConfig{}
+	project := &model.CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					Name: "other-service",
+				},
+				{
+					Name: "current-service",
+				},
+			},
+		},
+	}
+	// Execute test
+	result := isVolumeUsedByOtherServices(volume, service, project)
+	// Assert
+	assert.False(t, result)
 }
