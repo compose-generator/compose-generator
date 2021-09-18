03785ef4d08bbacd6adb84d29ba1d6f4f64099c5
Marc Auberer <marc.auberer@chillibits.com>
2021-09-18 13:19:11 +0000

    Add more tests

--- src/model/cg-project_test.go
+++ src/model/cg-project_test.go
@@ -111,8 +111,6 @@ func TestGetAllVolumePathsNormalized1(t *testing.T) {
 		},
 	}
 	expectedResult := []string{"./volumes/mysql-data", "/bin/compose-generator", "./volumes/wordpress-content"}
-	// Mock functions
-
 	// Execute test
 	result := project.GetAllVolumePathsNormalized()
 	// Assert
@@ -120,4 +118,76 @@ func TestGetAllVolumePathsNormalized1(t *testing.T) {
 	assert.EqualValues(t, expectedResult, result)
 }
 
+func TestGetAllVolumePathsNormalized2(t *testing.T) {
+	// Test data
+	project := CGProject{}
+	// Execute test
+	result := project.GetAllVolumePathsNormalized()
+	// Assert
+	assert.Zero(t, len(result))
+}
+
 // ----------------------------------------------------------------- GetAllEnvFilePaths ------------------------------------------------------------
+
+func TestGetAllEnvFilePaths1(t *testing.T) {
+	// Test data
+	project := &CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					EnvFile: []string{"./environment.env", "./volumes/wordpress/environment.env"},
+				},
+				{
+					EnvFile: []string{"./environment.env"},
+				},
+			},
+		},
+	}
+	expectedResult := []string{"./environment.env", "./volumes/wordpress/environment.env", "./environment.env"}
+	// Execute test
+	result := project.GetAllEnvFilePaths()
+	// Assert
+	assert.EqualValues(t, expectedResult, result)
+}
+
+func TestGetAllEnvFilePaths2(t *testing.T) {
+	// Test data
+	project := CGProject{}
+	// Execute test
+	result := project.GetAllEnvFilePaths()
+	// Assert
+	assert.Zero(t, len(result))
+}
+
+// ------------------------------------------------------------ GetAllEnvFilePathsNormalized -------------------------------------------------------
+
+func TestGetAllEnvFilePathsNormalized1(t *testing.T) {
+	// Test data
+	project := &CGProject{
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					EnvFile: []string{"./environment.env", "./volumes/wordpress/environment.env"},
+				},
+				{
+					EnvFile: []string{"./environment.env"},
+				},
+			},
+		},
+	}
+	expectedResult := []string{"./environment.env", "./volumes/wordpress/environment.env"}
+	// Execute test
+	result := project.GetAllEnvFilePathsNormalized()
+	// Assert
+	assert.Equal(t, 2, len(result))
+	assert.EqualValues(t, expectedResult, result)
+}
+
+func TestGetAllEnvFilePathsNormalized2(t *testing.T) {
+	// Test data
+	project := CGProject{}
+	// Execute test
+	result := project.GetAllEnvFilePathsNormalized()
+	// Assert
+	assert.Zero(t, len(result))
+}
