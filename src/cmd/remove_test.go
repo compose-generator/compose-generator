be5bbb4db48693d649fb8012e8b1d0ef8316097a
Marc Auberer <marc.auberer@chillibits.com>
2021-09-19 20:53:32 +0000

    Add more tests

--- src/cmd/remove_test.go
+++ src/cmd/remove_test.go
@@ -10,6 +10,19 @@ import (
 
 // ---------------------------------------------------------------------- Remove -------------------------------------------------------------------
 
+func TestRemove1(t *testing.T) {
+	/*// Test data
+	context := &cli.Context{
+
+	}
+	// Mock functions
+
+	// Execute test
+	Remove(context)
+	// Assert
+	*/
+}
+
 // ------------------------------------------------------------------ removeService ----------------------------------------------------------------
 
 func TestRemoveService1(t *testing.T) {
@@ -74,6 +87,79 @@ func TestRemoveService1(t *testing.T) {
 	assert.Equal(t, 1, removeNetworksPassCallCount)
 	assert.Equal(t, 1, removeDependenciesPassCallCount)
 	assert.Equal(t, expectedProject, project)
+}
+
+func TestRemoveService2(t *testing.T) {
+	// Test data
+	project := &model.CGProject{
+		ForceConfig: false,
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					Name: "service1",
+				},
+				{
+					Name: "service2",
+				},
+				{
+					Name: "service3",
+				},
+			},
+		},
+	}
+	serviceName := "service4"
+	// Mock functions
+	printErrorCallCount := 0
+	printError = func(description string, err error, exit bool) {
+		printErrorCallCount++
+		assert.Equal(t, "Service not found", description)
+		assert.NotNil(t, err)
+		assert.False(t, exit)
+	}
+	// Execute test
+	removeService(project, serviceName, false)
+	// Assert
+	assert.Equal(t, 1, printErrorCallCount)
+}
+
+func TestRemoveService3(t *testing.T) {
+	// Test data
+	project := &model.CGProject{
+		ForceConfig: false,
+		Composition: &spec.Project{
+			Services: spec.Services{
+				{
+					Name: "service1",
+				},
+				{
+					Name: "service2",
+				},
+				{
+					Name: "service3",
+				},
+			},
+		},
+	}
+	serviceName := "service2"
+	// Mock functions
+	printErrorCallCount := 0
+	printError = func(description string, err error, exit bool) {
+		printErrorCallCount++
+	}
+	yesNoQuestion = func(question string, defaultValue bool) bool {
+		assert.Equal(t, "Do you really want to remove service '"+serviceName+"'?", question)
+		assert.False(t, defaultValue)
+		return false
+	}
+	removeVolumesPassCallCount := 0
+	removeVolumesPass = func(service *spec.ServiceConfig, project *model.CGProject) {
+		removeVolumesPassCallCount++
+	}
+	// Execute test
+	removeService(project, serviceName, false)
+	// Assert
+	assert.Zero(t, printErrorCallCount)
+	assert.Zero(t, removeVolumesPassCallCount)
 }
 
 // ------------------------------------------------------------ removeServiceFromProject -----------------------------------------------------------
