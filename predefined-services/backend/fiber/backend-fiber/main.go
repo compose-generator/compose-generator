cff8185c7321db1eaa2fe81c6f3cb7496246decd
Marc Auberer <marc.auberer@chillibits.com>
2021-10-04 17:40:22 +0200

    Predefined service: Fiber (#147)
    * Add predefined service for Fiber
    
    * Add it to the supported services list
    
    * Add service README

---
+++ predefined-services/backend/fiber/backend-fiber/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
+}
