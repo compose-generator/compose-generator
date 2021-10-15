4fdff8bc4be8054ac4901135d41ee304f57054ff
Marc Auberer <marc.auberer@chillibits.com>
2021-10-15 19:33:37 +0000

    Test

--- .github/scripts/service-tester/predefined-service-tester.py
+++ .github/scripts/service-tester/predefined-service-tester.py
@@ -1,20 +1,22 @@
 """Script to test all combinations of predefined service templates"""
 
-import os
+from os.path import isdir, join
+from os import listdir, system
 import sys
 import itertools
 
-TEMPLATES_DIR = "../../../predefined-services"
-
 def get_all_template_names():
     """Returns a string array with all existing template names"""
-    template_types = list(filter(os.path.isdir, os.listdir(TEMPLATES_DIR)))
 
     template_names = []
-    for name in template_types:
-        template_names += list(filter(os.path.isdir, os.listdir(TEMPLATES_DIR + "/" + name)))
+    template_path = '../../../predefined-services'
+    template_types = ["backend", "database", "db-admin", "frontend"]
+    for template_type in template_types:
+        template_type_path = template_path + '/' + template_type
+        service_names = [f for f in listdir(template_type_path) if isdir(join(template_type_path, f))]
+        print(service_names)
+        template_names.extend(service_names)
 
-    print(template_names)
     return template_names
 
 def test_combination(comb):
@@ -23,17 +25,17 @@ def test_combination(comb):
     
 
     # Execute Compose Generator with the config file
-    if os.system("compose-generator -c config.yml -i") != 0:
+    if system("compose-generator -c config.yml -i") != 0:
         sys.exit('Compose Generator failed when generating stack for combination ' + comb)
 
     # Execute Compose Generator with the config file
-    if os.system("docker compose up -d") != 0:
+    if system("docker compose up -d") != 0:
         sys.exit('Docker failed when generating stack for combination ' + comb)
 
 def reset_environment():
     """Deletes all Docker related stuff. Should be executed after each test"""
-    os.system("docker system prune -af")
-    os.system("sudo rm -rf ./*")
+    system("docker system prune -af")
+    system("sudo rm -rf ./*")
 
 # Initially reset the testing environment
 print("Do initial cleanup ...", end='')
@@ -43,20 +45,20 @@ print(" done")
 # Find all possible template combinations
 print("Collecting template names ...", end='')
 templates = get_all_template_names()
-combinations = []
-for t in range(len(templates) +1):
-    combinations_list = list(itertools.combinations(templates, t))
-    combinations += combinations_list
-total = len(combinations)
-print(" done")
-
+#combinations = []
+#for i in range(1, len(templates) +1):
+#    combinations.append(list(itertools.combinations(templates, i)))
+#print(" done")
+#print(combinations)
+#
 # Execute test for each combination
-print("Execute tests ...", end='')
-for i, combination in enumerate(combinations):
-    print(f"Testing combination {i} of {total} ...")
-    #test_combination(combination)
-    reset_environment()
-print(" done")
+#print("Execute tests ...")
+#for i, combination in enumerate(combinations):
+#    print(f"Testing combination {i} of {len(combinations)} ...")
+    #if len(combination) > 0:
+    #    test_combination(combination)
+    #    reset_environment()
+#print("Done")
 
 # Test was successful
 print("Tested all combinations successfully!")
