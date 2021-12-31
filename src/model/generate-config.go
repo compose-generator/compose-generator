c88f842a4a2d91e66795af52d3ec8bcdb4b7bcf2
Marc Auberer <marc.auberer@chillibits.com>
2021-12-31 20:44:49 +0000

    Remove old copyright notice

--- src/model/generate-config.go
+++ src/model/generate-config.go
@@ -3,11 +3,6 @@ Copyright © 2021-2022 Compose Generator Contributors
 All rights reserved.
 */
 
-/*
-Copyright © 2021 Compose Generator Contributors
-All rights reserved.
-*/
-
 package model
 
 // GenerateConfig represents a configuration file, which can be passed to the generate command
