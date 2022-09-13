092edce36c22415cf7237f48ff232bd5428eac63
Marc Auberer <marc.auberer@chillibits.com>
2022-09-13 21:25:41 +0000

    Add predefined service template for PocketBase

---
+++ predefined-services/backend/pocketbase/README.md
@@ -0,0 +1,12 @@
+## PocketBase
+PocketBase is an open source backend, based on the Go programming language. It consists of:
+
+-   embedded database (SQLite) with realtime subscriptions
+-   built-in files and users management
+-   convenient Admin dashboard UI
+-   and simple REST-ish API
+
+PocketBase offers several sdk clients to interact with the backend.
+
+### Setup
+ToDo: Insert setup instructions here.
---
+++ predefined-services/backend/pocketbase/backend-pocketbase/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// add new "GET /api/hello" route to the app router (echo)
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/hello",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "Hello world!")
+			},
+			Middlewares: []echo.MiddlewareFunc{
+				apis.RequireAdminOrUserAuth(),
+			},
+		})
+
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
